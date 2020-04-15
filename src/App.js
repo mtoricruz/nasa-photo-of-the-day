78fe2f7a9c494e1c0c3ba7629c141d10b71348fe
toricruzmendiola <mtoricruz@gmail.com>
2020-04-15 15:35:19 -0700

    checkpoint save. added the file structure and added asset folder for pictures to add like the nasa logo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
+import NavigationSection from "./components/NavigationSection/navigation";
+
+const url = 'https://api.nasa.gov/planetary/apod?api_key='
+const api_key = '69vJxy2i2JM4jJSeMGboklPEKtx3ZCwaaLO4PSei'
 
 function App() {
+  const [nasaData, setNasaData] = useState(null)
+
+  useEffect(() => {
+    axios.get(`${url}${api_key}`)
+      .then(res => {
+        console.log(res.data)
+        setNasaData(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }, [])
+
   return (
+    
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <NavigationSection />
+      <div className="NASAcardImg">
+        {nasaData && <img src={nasaData.url} />}
+      </div>
+      <div className="titleAndDate">
+        
+      </div>
     </div>
   );
 }
---
+++ src/components/NavigationSection/navigation.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "./src/assets/nasalogo.png"
+
+const NavigationSection = () => {
+    return (
+        <div className="logo">
+            <img src={Logo}></img>
+        </div>
+    )
+}
+
+export default NavigationSection;

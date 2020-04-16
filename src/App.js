e204446f05744a0500c25c1d772543f1187c820f
toricruzmendiola <mtoricruz@gmail.com>
2020-04-15 17:34:37 -0700

    almost done structuring out the soon-to-be components and then i just need to pass data correctly and I'm done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from 'axios';
 import "./App.css";
 import NavigationSection from "./components/NavigationSection/navigation";
+import ImgSection from "./components/ImgSection/img";
 
 const url = 'https://api.nasa.gov/planetary/apod?api_key='
 const api_key = '69vJxy2i2JM4jJSeMGboklPEKtx3ZCwaaLO4PSei'
@@ -24,11 +25,15 @@ function App() {
     
     <div className="App">
       <NavigationSection />
-      <div className="NASAcardImg">
+       <div className="NASAcardImg">
         {nasaData && <img src={nasaData.url} alt='Pic of the Day'/>}
-      </div>
-      <div className="titleAndDate">
-        
+        </div> 
+      {/* <ImgSection imgURL={nasaData} />   */}
+      <div className="text-container">
+        <div className="title-and-date">
+            <h2>-title-</h2> 
+            <h5>-date-</h5>
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ImgSection/img.js
@@ -0,0 +1,12 @@
+// import React, { useState } from 'react'
+
+// const ImgSection = ({ imgURL }) => {
+//     const [imglink, setImgLink] = useState(imgURL.url)
+//     return (
+//         <div className="NASAcardImg">
+//             {nasaData && <img src={imgURL.url} alt='Pic of the Day'/>}
+//         </div>
+//     );
+// }
+
+// export default ImgSection
